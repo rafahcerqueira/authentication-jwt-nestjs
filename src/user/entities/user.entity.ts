7f7fa16e6d96fd7634a76b4857f8a0043cfbb4c9
Rafael <rafahcerqueira.123@gmail.com>
2023-07-06 18:34:12 -0300

    feat: adicionando post de usuario e findByEmail + modulo de auth

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,7 @@
+export class User {
+  name: string;
+  email: string;
+  password: string;
+  created_at: Date;
+  updated_at: Date;
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const data = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    };
+
+    const createdUser = await this.prisma.user.create({ data });
+
+    return {
+      ...createdUser,
+      password: undefined,
+    };
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+
+  update(id, updateUserDto: UpdateUserDto) {
+    return updateUserDto;
+  }
+
+  remove(id) {
+    return `This action removes a #${id} user`;
+  }
+}
