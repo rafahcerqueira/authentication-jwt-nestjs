ca5bdba721d110e9d9108b86f0816120cb422a09
Rafael <rafahcerqueira.123@gmail.com>
2023-07-07 17:27:04 -0300

    change: deixando rota de login publica

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,11 +9,13 @@ import {
 import { AuthService } from './auth.service';
 import { AuthRequest } from './models/AuthRequest';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { IsPublic } from './decorators/is-public.decorator';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @IsPublic()
   @Post('login')
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
